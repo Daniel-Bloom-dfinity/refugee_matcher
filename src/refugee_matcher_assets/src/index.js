3fc0fc665211b08fc3b97db809cff891db0d9725
Daniel.Bloom <daniel.bloom@dfinity.org>
2022-02-26 19:17:37 -0800

    Initial commit.

---
+++ src/refugee_matcher_assets/src/index.js
@@ -0,0 +1,19 @@
+import { refugee_matcher } from "../../declarations/refugee_matcher";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await refugee_matcher.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
