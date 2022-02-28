1042302c62180b2482f4204f0781b6898517c8e4
Daniel.Bloom <daniel.bloom@dfinity.org>
2022-02-28 10:31:29 -0800

    did

--- src/declarations/refugee_matcher/refugee_matcher.did.d.ts
+++ src/declarations/refugee_matcher/refugee_matcher.did.d.ts
@@ -1,33 +1,101 @@
 import type { Principal } from '@dfinity/principal';
-export interface Country { 'name' : string, 'regions' : Array<Region> }
-export type Duration = { 'Days' : null } |
-  { 'Weeks' : null } |
-  { 'Years' : null } |
-  { 'Months' : null };
-export interface HostV2 {
-  'foo' : number,
-  'duration' : Duration,
-  'active' : boolean,
-  'pets' : boolean,
-  'last_updated' : Time,
+export interface Host {
+  'duration' : { 'Days' : null } |
+    { 'Weeks' : null } |
+    { 'Months' : null },
+  'accepts_kids' : boolean,
+  'accepts_pets' : boolean,
+  'offer' : { 'Bed' : null } |
+    { 'RoomInHome' : null } |
+    { 'FullHouse' : null } |
+    { 'HotelHostel' : null } |
+    { 'FullApartment' : null } |
+    { 'Other' : string },
+  'accepts_women' : boolean,
+  'accepts_men' : boolean,
+  'description' : string,
+  'contant_info' : string,
   'start_date' : Time,
-  'rooms_available' : number,
+  'referral_code' : string,
+  'is_active' : boolean,
+  'spots_available' : number,
+  'location' : [bigint, bigint],
 }
 export interface HttpCounter {
-  'add_country' : (arg_0: Country) => Promise<bigint>,
-  'add_host' : (arg_0: HostV2) => Promise<bigint>,
-  'add_region' : (arg_0: bigint, arg_1: Region) => Promise<bigint>,
+  'add_country' : (arg_0: V1__2) => Promise<bigint>,
+  'add_region' : (arg_0: bigint, arg_1: V1) => Promise<bigint>,
+  'backup_hosts' : () => Promise<Array<V1__4>>,
+  'backup_refugees' : () => Promise<Array<V1__3>>,
   'check_admin' : () => Promise<boolean>,
-  'get_countries' : () => Promise<Array<Country>>,
-  'get_host' : (arg_0: bigint) => Promise<HostV2>,
+  'get_countries' : () => Promise<Array<V1__2>>,
   'set_country_name' : (arg_0: bigint, arg_1: string) => Promise<undefined>,
   'set_next_admin' : (arg_0: Principal) => Promise<undefined>,
   'switch_admin' : () => Promise<undefined>,
-  'update_region' : (arg_0: bigint, arg_1: bigint, arg_2: Region) => Promise<
+  'update_region' : (arg_0: bigint, arg_1: bigint, arg_2: V1) => Promise<
       undefined
     >,
+  'upsert_host' : (arg_0: Host) => Promise<undefined>,
+  'upsert_refuge' : (arg_0: Refugee) => Promise<undefined>,
 }
-export interface Location { 'micro_lat' : number, 'micro_long' : number }
-export interface Region { 'name' : string, 'location' : Location }
+export interface Refugee {
+  'people' : number,
+  'description' : string,
+  'start_window' : { 'Later' : null } |
+    { 'ThisWeek' : null } |
+    { 'ASAP' : null } |
+    { 'NextWeek' : null },
+  'contant_info' : string,
+  'referral_code' : string,
+  'location' : [bigint, bigint],
+  'includes_men' : boolean,
+  'includes_women' : boolean,
+  'includes_kids' : boolean,
+  'includes_pets' : boolean,
+}
+export type StartTimeWindowV1 = { 'Later' : null } |
+  { 'ThisWeek' : null } |
+  { 'ASAP' : null } |
+  { 'NextWeek' : null };
 export type Time = bigint;
+export interface V1 { 'name' : string, 'location' : V1__1 }
+export interface V1__1 { 'micro_lat' : number, 'micro_long' : number }
+export interface V1__2 { 'name' : string, 'regions' : Array<V1> }
+export interface V1__3 {
+  'owner' : Principal,
+  'people' : number,
+  'description' : string,
+  'last_updated' : Time,
+  'start_window' : StartTimeWindowV1,
+  'contant_info' : string,
+  'is_active' : boolean,
+  'location' : [bigint, bigint],
+  'includes_men' : boolean,
+  'includes_women' : boolean,
+  'includes_kids' : boolean,
+  'includes_pets' : boolean,
+}
+export interface V1__4 {
+  'duration' : { 'Days' : null } |
+    { 'Weeks' : null } |
+    { 'Months' : null },
+  'accepts_kids' : boolean,
+  'accepts_pets' : boolean,
+  'offer' : { 'Bed' : null } |
+    { 'RoomInHome' : null } |
+    { 'FullHouse' : null } |
+    { 'HotelHostel' : null } |
+    { 'FullApartment' : null } |
+    { 'Other' : string },
+  'owner' : Principal,
+  'accepts_women' : boolean,
+  'accepts_men' : boolean,
+  'description' : string,
+  'last_updated' : Time,
+  'contant_info' : string,
+  'start_date' : Time,
+  'referral_code' : string,
+  'is_active' : boolean,
+  'spots_available' : number,
+  'location' : [bigint, bigint],
+}
 export interface _SERVICE extends HttpCounter {}
--- src/declarations/refugee_matcher/refugee_matcher.did.js
+++ src/declarations/refugee_matcher/refugee_matcher.did.js
@@ -1,41 +1,116 @@
 export const idlFactory = ({ IDL }) => {
-  const Location = IDL.Record({
+  const V1__1 = IDL.Record({
     'micro_lat' : IDL.Int32,
     'micro_long' : IDL.Int32,
   });
-  const Region = IDL.Record({ 'name' : IDL.Text, 'location' : Location });
-  const Country = IDL.Record({
-    'name' : IDL.Text,
-    'regions' : IDL.Vec(Region),
-  });
-  const Duration = IDL.Variant({
-    'Days' : IDL.Null,
-    'Weeks' : IDL.Null,
-    'Years' : IDL.Null,
-    'Months' : IDL.Null,
-  });
+  const V1 = IDL.Record({ 'name' : IDL.Text, 'location' : V1__1 });
+  const V1__2 = IDL.Record({ 'name' : IDL.Text, 'regions' : IDL.Vec(V1) });
   const Time = IDL.Int;
-  const HostV2 = IDL.Record({
-    'foo' : IDL.Int32,
-    'duration' : Duration,
-    'active' : IDL.Bool,
-    'pets' : IDL.Bool,
+  const V1__4 = IDL.Record({
+    'duration' : IDL.Variant({
+      'Days' : IDL.Null,
+      'Weeks' : IDL.Null,
+      'Months' : IDL.Null,
+    }),
+    'accepts_kids' : IDL.Bool,
+    'accepts_pets' : IDL.Bool,
+    'offer' : IDL.Variant({
+      'Bed' : IDL.Null,
+      'RoomInHome' : IDL.Null,
+      'FullHouse' : IDL.Null,
+      'HotelHostel' : IDL.Null,
+      'FullApartment' : IDL.Null,
+      'Other' : IDL.Text,
+    }),
+    'owner' : IDL.Principal,
+    'accepts_women' : IDL.Bool,
+    'accepts_men' : IDL.Bool,
+    'description' : IDL.Text,
     'last_updated' : Time,
+    'contant_info' : IDL.Text,
     'start_date' : Time,
-    'rooms_available' : IDL.Nat16,
+    'referral_code' : IDL.Text,
+    'is_active' : IDL.Bool,
+    'spots_available' : IDL.Nat16,
+    'location' : IDL.Tuple(IDL.Nat, IDL.Nat),
+  });
+  const StartTimeWindowV1 = IDL.Variant({
+    'Later' : IDL.Null,
+    'ThisWeek' : IDL.Null,
+    'ASAP' : IDL.Null,
+    'NextWeek' : IDL.Null,
+  });
+  const V1__3 = IDL.Record({
+    'owner' : IDL.Principal,
+    'people' : IDL.Nat16,
+    'description' : IDL.Text,
+    'last_updated' : Time,
+    'start_window' : StartTimeWindowV1,
+    'contant_info' : IDL.Text,
+    'is_active' : IDL.Bool,
+    'location' : IDL.Tuple(IDL.Nat, IDL.Nat),
+    'includes_men' : IDL.Bool,
+    'includes_women' : IDL.Bool,
+    'includes_kids' : IDL.Bool,
+    'includes_pets' : IDL.Bool,
+  });
+  const Host = IDL.Record({
+    'duration' : IDL.Variant({
+      'Days' : IDL.Null,
+      'Weeks' : IDL.Null,
+      'Months' : IDL.Null,
+    }),
+    'accepts_kids' : IDL.Bool,
+    'accepts_pets' : IDL.Bool,
+    'offer' : IDL.Variant({
+      'Bed' : IDL.Null,
+      'RoomInHome' : IDL.Null,
+      'FullHouse' : IDL.Null,
+      'HotelHostel' : IDL.Null,
+      'FullApartment' : IDL.Null,
+      'Other' : IDL.Text,
+    }),
+    'accepts_women' : IDL.Bool,
+    'accepts_men' : IDL.Bool,
+    'description' : IDL.Text,
+    'contant_info' : IDL.Text,
+    'start_date' : Time,
+    'referral_code' : IDL.Text,
+    'is_active' : IDL.Bool,
+    'spots_available' : IDL.Nat16,
+    'location' : IDL.Tuple(IDL.Nat, IDL.Nat),
+  });
+  const Refugee = IDL.Record({
+    'people' : IDL.Nat16,
+    'description' : IDL.Text,
+    'start_window' : IDL.Variant({
+      'Later' : IDL.Null,
+      'ThisWeek' : IDL.Null,
+      'ASAP' : IDL.Null,
+      'NextWeek' : IDL.Null,
+    }),
+    'contant_info' : IDL.Text,
+    'referral_code' : IDL.Text,
+    'location' : IDL.Tuple(IDL.Nat, IDL.Nat),
+    'includes_men' : IDL.Bool,
+    'includes_women' : IDL.Bool,
+    'includes_kids' : IDL.Bool,
+    'includes_pets' : IDL.Bool,
   });
   const HttpCounter = IDL.Service({
-    'add_country' : IDL.Func([Country], [IDL.Nat], []),
-    'add_host' : IDL.Func([HostV2], [IDL.Nat], []),
-    'add_region' : IDL.Func([IDL.Nat, Region], [IDL.Nat], []),
+    'add_country' : IDL.Func([V1__2], [IDL.Nat], []),
+    'add_region' : IDL.Func([IDL.Nat, V1], [IDL.Nat], []),
+    'backup_hosts' : IDL.Func([], [IDL.Vec(V1__4)], []),
+    'backup_refugees' : IDL.Func([], [IDL.Vec(V1__3)], []),
     'check_admin' : IDL.Func([], [IDL.Bool], []),
-    'get_countries' : IDL.Func([], [IDL.Vec(Country)], ['query']),
-    'get_host' : IDL.Func([IDL.Nat], [HostV2], ['query']),
+    'get_countries' : IDL.Func([], [IDL.Vec(V1__2)], ['query']),
     'set_country_name' : IDL.Func([IDL.Nat, IDL.Text], [], ['oneway']),
     'set_next_admin' : IDL.Func([IDL.Principal], [], ['oneway']),
     'switch_admin' : IDL.Func([], [], ['oneway']),
-    'update_region' : IDL.Func([IDL.Nat, IDL.Nat, Region], [], ['oneway']),
+    'update_region' : IDL.Func([IDL.Nat, IDL.Nat, V1], [], ['oneway']),
+    'upsert_host' : IDL.Func([Host], [], ['oneway']),
+    'upsert_refuge' : IDL.Func([Refugee], [], ['oneway']),
   });
   return HttpCounter;
 };
-export const init = ({ IDL }) => { return []; };
+export const init = ({ IDL }) => { return [IDL.Principal]; };
